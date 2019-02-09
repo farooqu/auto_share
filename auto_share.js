05b0fc0021d1c8db00267a2818e3d98e764448e5
Umer Farooq <umer@thefarooqs.net>
2019-02-09 11:14:13 -0500

    Fix how comments are written

--- auto_share.js
+++ auto_share.js
@@ -23,14 +23,14 @@
     const shuffle = (array) => {
       let currentIndex = array.length, temporaryValue, randomIndex;
 
-      // While there remain elements to shuffle...
+      /* While there remain elements to shuffle... */
       while (0 !== currentIndex) {
 
-        // Pick a remaining element...
+        /* Pick a remaining element... */
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        // And swap it with the current element.
+        /* And swap it with the current element. */
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
