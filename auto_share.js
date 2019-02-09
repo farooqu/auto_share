7835f2b07953d821bf07d6d501a4d03c7cbfa2a9
Umer Farooq <umer@thefarooqs.net>
2019-02-09 11:02:41 -0500

    Added status div

--- auto_share.js
+++ auto_share.js
@@ -1,4 +1,6 @@
 (function(){
+    const statusDiv = document.createElement("div");
+    statusDiv.style.cssText = "position: fixed; top: 0px; left: 0px; z-index: 999; color: white; background: red; padding: 2px";
     const  ajaxSuccessEvent = "lprequestend";
     const inventoryTagClass = ".inventory-tag";
     const shareButtonClass = ".share";
@@ -6,7 +8,10 @@
     const followerShareClass = ".pm-followers-share-link";
 
     const getWindowHeight = () => document.body.offsetHeight;
-    const scrollToBottomOfPage = () => window.scrollTo(0, getWindowHeight());
+    const scrollToBottomOfPage = () => {
+        statusDiv.innerText = "Scrolling...";
+        window.scrollTo(0, getWindowHeight());
+    };
     const getAllTiles = () => document.querySelectorAll(".tile");
     const getActiveTiles = () => {
         const allTiles = getAllTiles();
@@ -35,21 +40,28 @@
     const getShareButton = t => t.querySelector(shareButtonClass);
 
     const shareActiveListings = () => {
+        statusDiv.innerText = "Starting to share items.";
+        document.body.appendChild(statusDiv);
         const shareModal = document.querySelector(shareModalId);
         const shareToFollowersButton = shareModal.querySelector(followerShareClass);
         const activeTiles = shuffle(getActiveTiles());
         let currentTileIndex = 0;
 
         const shareNextActiveTile = () => {
+            statusDiv.innerText = `Item ${currentTileIndex + 1} of ${activeTiles.length}, sharing...`;
             const currentTile = activeTiles[currentTileIndex++];
             const shareButton = getShareButton(currentTile);
 
             shareButton.click();
             shareToFollowersButton.click();
 
-            if(currentTileIndex < activeTiles.length){
+            if (currentTileIndex < activeTiles.length) {
                 let waitTime = Math.floor(Math.random() * Math.floor(4)) + 1;
                 window.setTimeout(shareNextActiveTile, waitTime * 1000);
+                statusDiv.innerText = `Item ${currentTileIndex + 1} of ${activeTiles.length}, shared, waiting...`;
+            } else {
+                window.alert("All Done Meri Jaan! I love you!");
+                statusDiv.remove();
             }
         };
         shareNextActiveTile();
@@ -58,6 +70,7 @@
     let lastWindowHeight = getWindowHeight();
 
     const checkHeightAndScroll = () => {
+        statusDiv.innerText = "Checking Height...";
         const newHeight = getWindowHeight();
         if (newHeight !== lastWindowHeight){
             lastWindowHeight = newHeight;
