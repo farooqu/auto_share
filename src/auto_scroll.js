575a9d58ed650c9679b99a2ddeabb904ce341315
Umer Farooq <umer@thefarooqs.net>
2020-10-26 20:22:01 -0400

    Add status for scrolling

--- src/auto_scroll.js
+++ src/auto_scroll.js
@@ -1,16 +1,22 @@
 (function () {
+  const statusDiv = document.createElement("div");
+  statusDiv.style.cssText = `position: fixed; top: 0px; left: 0px; z-index: 9999; color: white; background: orange; padding: 2px`;
+  document.body.appendChild(statusDiv);
+
   const isLoading = () =>
     !!document.querySelector("#infinite-scroll")?.offsetParent;
 
   const getWindowHeight = () => document.body.offsetHeight;
 
   const scrollToBottomOfPage = () => {
+    statusDiv.innerText = "Scrolling...";
     window.scrollTo(0, getWindowHeight());
   };
 
   let lastWindowHeight;
   const intervalId = window.setInterval(() => {
     if (!isLoading()) {
+      statusDiv.innerText = "Checking Height...";
       const newHeight = getWindowHeight();
       if (newHeight !== lastWindowHeight) {
         lastWindowHeight = newHeight;
